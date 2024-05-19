c02e672c2ea3d26d2d27a8c3ef335e60e1c7ac2a
codebybablu <bablu.mailaxy@gmail.com>
2024-05-19 16:30:07 +0530

    this is my first f2c project

---
+++ script.js
@@ -0,0 +1,20 @@
+let c = document.getElementById('cel');
+let f = document.getElementById('far');
+
+c.addEventListener('input', function(){
+    let celsius = c.value;
+    let faren = celsius * (9/5) + 32;
+    f.value = faren.toFixed(2);
+    if(c.value == ""){
+        f.value = "";
+    }
+});
+
+f.addEventListener('input', function(){
+    let frnht = f.value;
+    let cc = (frnht - 32) * 5/9;
+    c.value = cc.toFixed(2);
+    if(f.value == ""){
+        c.value = "";
+    }
+});
